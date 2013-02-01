0d2837868c3255ccc893864111d2154464fc445b
tsx525 <tsx525@gmail.com>
2013-01-31 16:09:59 -0800

    Removed unneeded caller definitions

--- src/main/Main.cpp
+++ src/main/Main.cpp
@@ -18,17 +18,14 @@ int main()
     switch (caller)
     {
         case 1:               //Calls login()
-            caller = 1;
             login();
             main();
             break;
         case 2:               //Calls cmd_line()
-            caller = 1;
             cmd_line();
             main();
             break;
         case 3:               //Calls new_user()
-            caller = 1;
             new_user();
             main();
             break;
